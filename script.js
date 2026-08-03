3709e8c503bdb8967aab82b809314909b1f31d24
haseebgotome-crypto <haseebgotome@gmail.com>
2026-08-03 21:29:31 +0500

    Update script.js

--- script.js
+++ script.js
@@ -21,3 +21,35 @@ button.addEventListener("click", () => {
         photo.src = images[current];
     }, 3000);
 });
+const canvas = document.getElementById("fireworks");
+const ctx = canvas.getContext("2d");
+
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+window.addEventListener("resize", () => {
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+});
+
+function fireworks() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+    for (let i = 0; i < 40; i++) {
+        ctx.beginPath();
+        ctx.arc(
+            Math.random() * canvas.width,
+            Math.random() * canvas.height,
+            Math.random() * 4 + 2,
+            0,
+            Math.PI * 2
+        );
+
+        ctx.fillStyle = `hsl(${Math.random() * 360},100%,60%)`;
+        ctx.fill();
+    }
+
+    requestAnimationFrame(fireworks);
+}
+
+fireworks();
